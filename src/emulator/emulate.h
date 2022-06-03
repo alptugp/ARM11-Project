b1c1fa75ee21e001cc2e666350204167bb664458
adi4656 <56588414+adi4656@users.noreply.github.com>
2022-06-03 18:33:58 +0100

    (Author all) Add definitions to emulate.h

--- src/emulator/emulate.h
+++ src/emulator/emulate.h
@@ -3,6 +3,7 @@
 
 
 #define NUM_GENERAL_PURPOSE_REGISTERS 13
+#define REGISTER_ADDRESS_LENGTH 4
 
 #define COND_MSB 31
 #define COND_LSB 28
@@ -13,6 +14,7 @@
 
 
 typedef uint32_t word;
+typedef int32_t signed_word;
 typedef uint8_t byte;
 typedef int8_t memory_t[];
 
