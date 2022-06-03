8f047b5ed5f117e3dbe5d6d809d8ae730845e7f8
adi4656 <56588414+adi4656@users.noreply.github.com>
2022-06-03 18:26:42 +0100

    Complete untested code for execute stage in pipeliner

--- src/emulator/pipeliner/pipeliner.h
+++ src/emulator/pipeliner/pipeliner.h
@@ -13,6 +13,6 @@
 #define TERMINATE_VALUE 0
 
 
-void pipeline(memory_t main_memory, struct RegisterFile registers);
+void pipeline(memory_t main_memory, struct RegisterFile *registers);
 static instruction_ptr decode(word instruction);
 static void execute(word instruction, word instructionCode);
