976a2ef28b511b36b8ea3ee9190de2720c8ed6bc
adi4656 <56588414+adi4656@users.noreply.github.com>
2022-06-03 04:51:44 +0100

    (Author Adi) Move pipeliner code into separate subdirectory
    Emulator subdirectory getting too messy: hard to navigate

--- src/emulator/test.c
+++ src/emulator/test.c
@@ -1,5 +1,3 @@
-#include "pipeliner.h"
-#include "pipeliner.c"
 #include "emulate.h"
 // #include "emulate.c"
 #include <assert.h>
@@ -12,15 +10,6 @@ void test_extractBits() {
     assert(extract_bits(305419896, 8, 12) == 86);
     assert(extract_bits(305419896, 12, 16) == 120);
     printf("All tests passed for extract_bits\n");
-}
-
-// a unit test that asserts that decode works
-// MAKE THIS ACTUALLY WORK!
-void test_decode() {
-    assert(decode(0x12345678) == &single_data_transfer);
-    assert(decode(0x12345678) == &branch);
-    assert(decode(0x12345678) == &data_processing);
-    printf("All tests passed for decode\n");
 }
 
 // a unit test that asserts that cond_check works
