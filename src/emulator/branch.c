0e00a14aaf3364b40a6e98f84318a224d55d6447
adi4656 <56588414+adi4656@users.noreply.github.com>
2022-06-03 21:49:37 +0100

    (Author Alptug&Krish&Adi) Complete branch code and simplify handling of signs

--- src/emulator/branch.c
+++ src/emulator/branch.c
@@ -1,23 +1,28 @@
 #include "branch.h"
 
-word branch(word *instruction, struct RegisterFile *registers, const memory_t memory) { 
+word branch(word *instruction, struct RegisterFile *registers) { 
     if (cond_check(*instruction, registers) == 1) {
-        word shifted_offset = (extract_bits(*instruction, OFFSET_LSB, OFFSET_MSB)) <<= SHIFT_VALUE_OFFSET;
-        offset_extend(shifted_offset, OFFSET_SIGN_BIT, OFFSET_SIGN_BIT);
-        word pc_value = shifted_offset + registers->program_counter;
-        is_negative(pc_value) ? printf("Cannot be branched to a negative instruction address.\n") : registers->program_counter = pc_value;
-        return 1;
+        signed_word offset = (signed_word) (extract_bits(*instruction, OFFSET_LSB, OFFSET_MSB));
+        offset <<= SHIFT_VALUE_OFFSET;
+        // offset_extend(shifted_offset, OFFSET_SIGN_BIT, OFFSET_SIGN_BIT);
+        signed_word pc_value = offset + (signed_word) registers->program_counter;
+        if((pc_value < 0)) {
+            printf("Cannot be branched to a negative instruction address.");
+            assert(0);
+        }
+        registers->program_counter = pc_value;
     }
     return 0;
 }
 
+/*
 int offset_extend(word shifted_offset, short offset_sign_bit) {
     sign_bit = extract_bits(shifted_offset, offset_sign_bit, offset_sign_bit);
     if (offset_sign_bit == 1) {
         shifted_offset |= SIGNED_OFFSET_EXTEND; //Should we define SINGED_OFFSET_EXTEND as MASK_FOR_SIGNED_OFFSET
     }
 }
-
 int is_negative(word instruction_address) {
     return extract_bits(instruction_address, INSTRUCTION_ADDRESS_SIGN_BIT, INSTRUCTION_ADDRESS_SIGN_BIT) == 1;
 }
+*/
--- src/emulator/branch.h
+++ src/emulator/branch.h
@@ -4,12 +4,12 @@
 #include <stdint.h>
 #include "emulate.h"
 
-#define OFFSET_SIZE 22
-#define OFFSET_START 0
+#define OFFSET_MSB 23
+#define OFFSET_LSB 0
 #define SIGNED_OFFSET_EXTEND 0xfc000000
 #define SHIFT_VALUE_OFFSET 2
 #define SIGN_BIT_OFFSET 23
 
-word branch(word instruction, struct RegisterFile *registers);
+word branch(word *instruction, struct RegisterFile *registers);
 
 #endif
