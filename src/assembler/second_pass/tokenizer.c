024fa6f3b8532da0c45e0904bee79d512f36f9ab
adi4656 <56588414+adi4656@users.noreply.github.com>
2022-06-19 00:11:34 +0100

    (Danish,Adi,Alptug) Fix tokenizer

--- src/assembler/second_pass/tokenizer.c
+++ src/assembler/second_pass/tokenizer.c
@@ -1,7 +1,50 @@
 #include "tokenizer.h"
-#include "./first_pass/first_pass.h"
 
-// A tokenizer for breaking a line into its label, opcode and operand field(s) 
+// Removes commas from a token
+// E.g. "R0, " -> "R0"
+static char *remove_token_commas(char *str) {
+    char *ptr = str;
+    while (*ptr) {
+        if (*ptr == ',') {
+            *ptr = '\0';
+            break;
+        }
+        ptr++;
+    }
+    return str;
+}
+
+// Trims leading and trailing whitespace from a string. 
+static char *trim_whitespace(char *str) {
+
+    // Trim leading space
+    while(*str == ' ') {
+        str++;
+    }
+
+    // Trim trailing space
+    char *end = str + strlen(str) - 1;
+    while(end > str && *end == ' ') {
+        end--;
+    }
+
+    *(end+1) = '\0';
+
+    return str;
+}
+
+// Removes comments from a line in assembly code
+static char *remove_comments(char *str) {
+    char* ptr = str;
+    while (*ptr) {
+        if (*ptr == ';') {
+            *ptr = '\0';
+            break;
+        }
+        ptr++;
+    }
+    return str;
+}
 
 tokenized_source_code tokenize_line(char* line) {
     line = trim_whitespace(line);
@@ -13,27 +56,33 @@ tokenized_source_code tokenize_line(char* line) {
         tokens.size = 0;
         return tokens;
     }
+    /*
     if (is_label(line)) {
         tokenized_source_code result;
         result.string_array = get_label(line);
         result.size = NELEMS(get_label(line));
         return result;
     }
+    */
 
     tokenized_source_code tokens;
+    tokens.string_array = malloc(sizeof(char *) * MAX_NUMBER_TOKENS);
     int tokens_index = 0;
-    char* token = strtok(line, " ");
+    char *token = strtok(line, TOKEN_DELIMITERS);
     while (token != NULL) {
-        token = strtok(NULL, " ");
         remove_token_commas(token);
         trim_whitespace(token);
+        tokens.string_array[tokens_index] = malloc(sizeof(char) * MAX_TOKEN_LENGTH);
         tokens.string_array[tokens_index] = token;
         tokens_index++;
+        token = strtok(NULL, TOKEN_DELIMITERS);
     }
     tokens.string_array[tokens_index] = NULL;
+    tokens.size = tokens_index - 1;
     return tokens;
 }
 
+/*
 tokenized_source_code* tokenize_file(char** input) {
     tokenized_source_code* output = malloc(sizeof(tokenized_source_code) * NELEMS(input));
     int i = 0;
@@ -45,7 +94,9 @@ tokenized_source_code* tokenize_file(char** input) {
     output->size = i;
     return output;
 }
+*/
 
+/*
 // Function to check if a line is a label 
 bool is_label(char *line) {
     trim_whitespace(line);
@@ -68,51 +119,4 @@ char* get_label(char* line) {
     // line(strlen(line) - 1) = NULL;
     // return line;
 }
-
-
-// Removes commas from a token
-// E.g. "R0, " -> "R0"
-char* remove_token_commas(char* str) {
-    char* ptr = str;
-    while (*ptr != NULL) {
-        if (*ptr == ',') {
-            *ptr = NULL;
-            break;
-        }
-        ptr++;
-    }
-    return str;
-}
-
-// Trims leading and trailing whitespace from a string. 
-char* trim_whitespace(char* str) {
-
-    // Trim leading space
-    while(isspace(*str)) {
-        str++;
-    }
-
-    // Trim trailing space
-    char *end = str + strlen(str) - 1;
-    while(end > str && isspace(*end)) {
-        end--;
-    }
-
-    // Write new null terminator
-    *(end+1) = NULL;
-
-    return str;
-}
-
-// Removes comments from a line in assembly code
-char* remove_comments(char* str) {
-    char* ptr = str;
-    while (*ptr != NULL) {
-        if (*ptr == ';') {
-            *ptr = NULL;
-            break;
-        }
-        ptr++;
-    }
-    return str;
-}
+*/
--- src/assembler/second_pass/tokenizer.h
+++ src/assembler/second_pass/tokenizer.h
@@ -2,19 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "./utils.h"
+#include "../utils.h"
 
 
 
 #define MAX_NUMBER_TOKENS 100
 #define MAX_LINE_CHRACTERS 511
 #define MAX_TOKEN_LENGTH 100
+#define TOKEN_DELIMITERS " ,"
 
-char* get_label(char* line);
-bool is_label(char *line);
-char* trim_whitespace(char* str);
-char* remove_comments(char* str);
-bool is_label(char *line);
-char* get_label(char* line);
 tokenized_source_code tokenize_line(char* line);
-tokenized_source_code* tokenize_file(char** input);
+// tokenized_source_code* tokenize_file(char** input);
