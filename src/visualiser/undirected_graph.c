94d5b2acfb0ac24937f8c512341df840e2afd0e6
Krish Maha <krish@mahas.net>
2022-06-22 18:32:05 +0100

    (Krish,Alptug) Implement undirected graph initialiser

--- src/visualiser/undirected_graph.c
+++ src/visualiser/undirected_graph.c
@@ -2,7 +2,6 @@
 #include "utils.h"
 #include <assert.h>
 
-/*
 adj_list_node *create_adj_list_node(int node) {
     adj_list_node *new_node = malloc(sizeof(adj_list_node));
     new_node->node = node;
@@ -10,9 +9,9 @@ adj_list_node *create_adj_list_node(int node) {
     return new_node;
 }
 
-undirected_graph_t *create_undirected_graph(int size) {
+undirected_graph *create_undirected_graph(int size) {
     // Allocate memory for the graph
-    undirected_graph_t *new_graph = malloc(sizeof(undirected_graph_t));
+    undirected_graph *new_graph = malloc(sizeof(undirected_graph));
 
     // Initialise the graph
     new_graph->size = size;
@@ -26,7 +25,7 @@ undirected_graph_t *create_undirected_graph(int size) {
     return new_graph;
 }
 
-void add_arc(undirected_graph_t *graph, int source, int target) {
+void add_arc(undirected_graph *graph, int source, int target) {
     assert(source >= 0);
     assert(source < graph->size);
     assert(target >= 0);
@@ -71,7 +70,7 @@ void add_arc(undirected_graph_t *graph, int source, int target) {
 
 }
 
-int get_num_arcs(undirected_graph_t *graph) {
+int get_num_arcs(undirected_graph *graph) {
     int num_arcs = 0;
     for (int i = 0; i < graph->size; i++) {
         num_arcs += graph->adj_list[i].size;
@@ -80,11 +79,11 @@ int get_num_arcs(undirected_graph_t *graph) {
     return num_arcs / 2;
 }
 
-int get_num_nodes(undirected_graph_t *graph) {
+int get_num_nodes(undirected_graph *graph) {
     return graph->size;
 }
 
-bool has_arc(undirected_graph_t *graph, int source, int target) {
+bool has_arc(undirected_graph *graph, int source, int target) {
     assert(source >= 0);
     assert(source < graph->size);
     assert(target >= 0);
@@ -114,7 +113,7 @@ bool has_arc(undirected_graph_t *graph, int source, int target) {
     return source_has_target && target_has_source;
 }
 
-void free_undirected_graph(undirected_graph_t *graph) {
+void free_undirected_graph(undirected_graph *graph) {
     for (int i = 0; i < graph->size; i++) {
         adj_list_node *current_node = graph->adj_list[i].head;
         while (current_node != NULL) {
@@ -127,13 +126,13 @@ void free_undirected_graph(undirected_graph_t *graph) {
     free(graph);
 }
 
-int get_degree(undirected_graph_t *graph, int vertex) {
+int get_degree(undirected_graph *graph, int vertex) {
     assert(vertex >= 0);
     assert(vertex < graph->size);
     return graph->adj_list[vertex].size;
 }
 
-void print_undirected_graph_adj_list(undirected_graph_t *graph) {
+void print_undirected_graph_adj_list(undirected_graph *graph) {
     for (int i = 0; i < graph->size; i++) {
         adj_list_node *current_node = graph->adj_list[i].head;
         printf("%d: ", i);
@@ -144,18 +143,72 @@ void print_undirected_graph_adj_list(undirected_graph_t *graph) {
         printf("\n");
     }
 }
-*/
+
 
 undirected_graph_t initialise_undirected(void) {
-    char size[MAX_SIZE];
-    fgets(size, MAX_SIZE, stdin);
+    undirected_graph_t *graph = malloc(sizeof(undirected_graph_t));
 
-    char adj_matrix[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE];
-    fgets(adj_matrix[MAX_MATRIX_SIZE], sizeof(*adj_matrix), stdin);
+    printf("Enter the number of nodes of the graph.\n");
+    int num_nodes;
+    fgets(num_nodes, sizeof(num_nodes), stdin);
+    strip_trailing_newline(num_nodes);
+    graph->num_nodes = num_nodes;
 
-    fgets(num_visited_arcs, NUM_VISITED_ARCS, stdin);
+    //Sets the array for all the nodes
+    undirected_node *node_arr[num_nodes];
 
+    for (int i = 0; i < num_nodes; i++) {
+        node_arr[i] = malloc(sizeof(undirected_node));
+        node_arr[i]->index = i;
+    }
+
+    graph->node_arr = node_arr;
+
+    // Sets the adjacency matrix for the graph
+    int adj_matrix[num_nodes][num_nodes];
+
+    for (int row = 0; row < num_nodes; row++) {
+        for (int col = 0; col < num_nodes; col++) {
+            char *answer;
+            printf("Is there an arc between the nodes with indices %d and %d? y/n\n", row, col);
+            fgets(answer, sizeof(char), stdin);
+            strip_trailing_newline(answer);
+            if (strcmp(answer, "y") == 0) {
+                adj_matrix[row][col] = 1;
+            } else if (strcmp(answer, "n") == 0) {
+                adj_matrix[row][col] = 0;
+            } else {
+                printf("Please enter y/n.\n");
+            }
+        }
+    }
+
+    graph->adj_matrix = adj_matrix;
+
+    // Sets the array for visited nodes
+    bool *visited[num_nodes];
+
+    for (int i = 0; i < num_nodes; i++) {
+        visited[i] = false;
+    }
+
+    graph->visited = visited;
+
+    // Sets the linked list for visited arcs
+    undirected_arc *visited_arcs = NULL;
+    graph->visited_arcs = visited_arcs;
     
+    //num_visited_arcs is set to zero as we are initialising the graph.
+    int num_visited_arcs = 0;
     
+
+    // Asks user the index of the current node nd sets it
+    int *current_node_index;
+    printf("Enter the index of the node which you want to be the current node.\n");
+    fgets(current_node_index, sizeof(int), stdin);
+    strip_trailing_newline(current_node_index);   
+    undirected_node *current_node = malloc(sizeof(undirected_node));
+    current_node->index = current_node_index;
 
 }
+
